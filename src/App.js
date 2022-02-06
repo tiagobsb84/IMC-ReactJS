1a29f3c4afeb01abd3aa72870f21aace61f1d33e
Tiago Silva <tiagobsb84@gmail.com>
2022-02-06 17:24:16 -0300

    Interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import "./app.css";
+
 export default function App(){
   return(
     <div className="app">
       <h1>Calculadora IMC</h1>
       <span>Vamos calcular seu imc</span>
-      
-    
+      <div className="area-input">
+        <input type="text" placeholder="Peso em (kg)" />
+        <input type="text" placeholder="Altura em (cm)" />
+
+        <button className="app-button">Calcular</button>
+      </div>  
+      <h2>Seu imc foi 25, você está abaixo do peso!</h2> 
     </div>
   )
 }
